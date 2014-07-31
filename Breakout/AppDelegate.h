e5a2aef4c84ed90f57d5b1b607ecb048a9666507
Glen Ruhl <glenruhl@Glens-MacBook-Air.local>
2014-07-31 15:11:51 -0500

    Initial Commit

---
+++ Breakout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Breakout
+//
+//  Created by Glen Ruhl on 7/31/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
